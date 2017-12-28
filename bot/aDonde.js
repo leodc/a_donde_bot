c5ddcdebfb95973f14e9306b2523f2aedc5ab1bb
Leonel Castañeda <leodcastaneda@gmail.com>
2017-12-28 13:35:16 -0600

    fixup! bot response

--- bot/aDonde.js
+++ bot/aDonde.js
@@ -113,7 +113,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
+    qs: { access_token: process.env.A_DONDE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
   }, function (error, response, body) {
